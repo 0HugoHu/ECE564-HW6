63c6e97550f04ede2962aabf9f9c26cc2efed5fc
yh342 <yh342@duke.edu>
2023-09-28 17:29:25 -0400

    Initial Commit

---
+++ ListView/ListViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListViewApp.swift
+//  ListView
+//
+//  Created by Hugooooo on 9/28/23.
+//
+
+import SwiftUI
+
+@main
+struct ListViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

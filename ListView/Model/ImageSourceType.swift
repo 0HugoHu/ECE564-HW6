20faab9d74456098586c898533c4bdf21015ced6
yh342 <yh342@duke.edu>
2023-10-09 22:24:42 -0400

    hw5

---
+++ ListView/Model/ImageSourceType.swift
@@ -0,0 +1,8 @@
+//
+//  ImageSourceType.swift
+//  Hugo
+//
+//  Created by Hugooooo on 10/9/23.
+//
+
+import Foundation
---
+++ ListView/View/Item/CircleText.swift
@@ -0,0 +1,8 @@
+//
+//  CircleText.swift
+//  Hugo
+//
+//  Created by Hugooooo on 10/8/23.
+//
+
+import Foundation
